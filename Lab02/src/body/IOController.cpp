e43b42c49c47c5526e31f057a66146ca17e42db8
Yannick Brilmayer <yannick.brilmayer@stud.h-da.de>
2021-07-03 22:54:49 +0200

    changed absolute path

--- Lab02/src/body/IOController.cpp
+++ Lab02/src/body/IOController.cpp
@@ -16,7 +16,7 @@ void IOController::start() {
 
     // tmp!
     std::vector<string> fileNames;
-    fileNames.push_back("/home/philipp/Documents/Compilerbau/Y-Raster-A/Lab02/inputFiles/correct4.go");
+    fileNames.push_back(inputDirectory + "correct4.go");
     /*fileNames.push_back(inputDirectory + "correct1.go");
     fileNames.push_back(inputDirectory + "correct2.go");
     fileNames.push_back(inputDirectory + "correct3.go");
